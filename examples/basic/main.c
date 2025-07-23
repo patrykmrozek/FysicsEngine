1fe3474485c46482b445626a4160e7426998cc8d
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2025-07-23 18:47:12 +0100

    cglm setup

--- examples/basic/main.c
+++ examples/basic/main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
-#include "../../include/physics/math/vector2.h"
+#include <cglm/cglm.h>
+#include <cglm/struct.h>
 
 int main() {
     //testing the vector2 functions
     
-    vec2_t a = {100, 50};
-    vec2_t b = {75, 20};
-    vec2_t c = vec2_add(a, b);
+    vec2s a = {100, 50};
+    vec2s b = {75, 20};
+    vec2s c = glms_vec2_add(a, b);
+    vec2s d = glms_vec2_sub(a, b);
+    vec2s e = glms_vec2_scale(a, 5);
 
     printf("Addition: %f, %f\n", c.x, c.y);
+    printf("Subtraction: %f, %f\n", d.x, d.y);
+    printf("Scale: %f, %f", e.x, e.y);
+
 
 
     return 0;
