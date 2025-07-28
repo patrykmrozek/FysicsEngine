bce6e4e861dd7d676cbc86726c287f81a76883ab
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2025-07-28 23:45:12 +0100

    updated fyCollisionInfo for CircleVsCircle collisions

--- src/collision/collision_detection.c
+++ src/collision/collision_detection.c
@@ -1,14 +1,27 @@
 #include <physics/collision/collision_detection.h>
 
 bool fyCollision_CircleVsCircle(fyBody* a, fyBody* b, fyCollisionInfo* output) {
-    float dist = fyVec2_DistSquared(a->position, b->position); //dist between centers
+    float dist_sq = fyVec2_DistSquared(a->position, b->position); //dist between centers
     float sum_r = a->shape.data.circle.radius + b->shape.data.circle.radius; //sum radii 
-    
-    
 
-    return (dist <= sum_r*sum_r);
+    if (dist_sq <= sum_r*sum_r) { //if there is a collision
+        float dist = sqrt(dist_sq);
+        output->depth = sum_r - dist; //penetration depth
+
+        if (dist > EPSILON) {
+            fyVec2 direction = fyVec2_Sub(b->position, a->position);
+            output->normal = fyVec2_Normal(direction);
+        } else {
+            output->normal = (fyVec2){0.0f, 0,0f}; //default direction
+        }
+
+        fyVec2 offset = fyVec2_Scale(output->normal, a->shape.data.circle.radius);
+        output->contact_point = fyVec2_Add(a->position, offset);
+
+        return true;
+    }
+    return false;
 }
-
 
 bool fyCollision_RectVsRect(fyBody* a, fyBody* b, fyCollisionInfo* output) {
     fyAABB aabb_a = fyBody_GetAABB(a);
@@ -16,6 +29,10 @@ bool fyCollision_RectVsRect(fyBody* a, fyBody* b, fyCollisionInfo* output) {
     
     //since I dont have rotations inplemented yet, the AABB check IS the collision check
     if (fyAABB_TestOverlap(aabb_a, aabb_b)) {
+
+        //TODO: Update fyCollisionInfo
+
+
         return true;
     }
     return false;
@@ -32,6 +49,10 @@ bool fyCollision_CircleVsRect(fyBody* circle, fyBody* rect, fyCollisionInfo* inf
     float dist = fyVec2_DistSquared(circle->position, (fyVec2){closest_x, closest_y});
 
     if (dist <= r*r) {
+
+        //TODO: Update fyCollisionInfo
+
+
         return true; //collision
     }
     return false;
